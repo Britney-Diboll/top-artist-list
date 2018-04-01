d7d048219321a1bed913da7c603b5f6d356eb61d
Diboll <bdiboll@proforma.com>
2018-04-01 19:41:23 -0400

    added some js

--- public/main.js
+++ public/main.js
@@ -1,5 +1,3 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
 
-document.addEventListener('DOMContentLoaded', main)
+const URL = "http://ws.audioscrobbler.com/2.0/"
+const API_KEY = "d196b5b43027b5e3de5ed4de20c757b2"
