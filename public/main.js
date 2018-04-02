c73c15f4f459e56b1c36662f15fcd6978a77a546
Diboll <bdiboll@proforma.com>
2018-04-02 12:32:11 -0400

    angular not working

--- public/main.js
+++ public/main.js
@@ -1,3 +1,19 @@
+const URL ="http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=d196b5b43027b5e3de5ed4de20c757b2&format=json"
 
-const URL = "http://ws.audioscrobbler.com/2.0/"
-const API_KEY = "d196b5b43027b5e3de5ed4de20c757b2"
+angular 
+  .module("mainApp", [])
+  .controller("mainController", ["$scope", "$http", ($scope, $http) => {
+
+
+    $http({
+      method: "GET",
+      url : URL,
+    }).then(response => {
+      console.log(response);
+      $scope.data = response.data.artists.artist;
+      console.log(response.data);
+    })
+    
+  }])
+
+  
